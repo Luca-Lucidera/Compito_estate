0d566c025f2fd7a63339229bfa9b90fd3f6bf88d
Lucidera Luca <lucidera.luca@gmail.com>
2021-07-29 14:10:27 +0200

    Progetto base con sln

---
+++ Prototipo/Project T/Project T/Tata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_T
+{
+    public class Tata
+    {
+        public int id { set; get; }
+        public string nome { set; get; }
+        public string cognome { set; get; }
+        public string email { set; get; }
+        public string zona_operativa { set; get; }
+        public bool occupata { set; get; }
+        public string image_path { set; get; }
+        public override string ToString()
+        {
+            string tmp="";
+            if (occupata)
+                tmp = "occupata";
+            else
+                tmp = "libera";
+            return "id: " + id + " nome " + nome + " cognome " + cognome + " zona operativa: " + zona_operativa + " occupazione: " + tmp;
+        }
+    }
+}
---
+++ Prototipo/Project T/Project T/Utente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_T
+{
+    class Utente
+    {
+        public int id { set; get; }
+        public string nome { set; get; }
+        public string cognome { set; get; }
+        public string email { set; get; }
+        public string nascita { set; get; }
+        public override string ToString()
+        {
+            return "id: " + id + " nome: " + nome + " email: " + email + " nascita: "+ nascita;
+        }
+    }
+}
